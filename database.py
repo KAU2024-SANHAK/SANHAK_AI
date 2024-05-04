3e2c8ccfe2a7540000834edf8b663334e578736b
lif_eposi <104194878+lifeposi@users.noreply.github.com>
2024-05-04 09:58:06 +0900

    추적중지

--- database.py
+++ database.py
@@ -8,7 +8,7 @@ from flask_cors import CORS
 
 
 app = Flask(__name__)
-CORS(app, supports_credentials=True, origins=('http://localhost:5173', 'https://honeyary.vercel.app/'))
+CORS(app, supports_credentials=True, origins=('http://localhost:5173/', 'https://honeyary.vercel.app/'))
 
 
 mysql_params = {
