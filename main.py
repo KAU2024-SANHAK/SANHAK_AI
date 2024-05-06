8800de875ff7ecbc2b11798c3fa0afe8980f566f
Wang HoEun <hoeunwang@gmail.com>
2024-05-06 18:04:20 +0900

    fix: 헤더 허용 범위 수정해주기

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT","PATCH", "DELETE"],
-    allow_headers=["*"],
+    allow_headers=["Content-Type", "Authorization"],
 )
 
 mysql_params = {
