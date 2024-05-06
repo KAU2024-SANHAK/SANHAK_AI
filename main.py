9cf9b45df9978a3952a1d9cffddde982ceb25a16
Wang HoEun <hoeunwang@gmail.com>
2024-05-06 15:23:50 +0900

    style: cors url 추가

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ origins = [
     "http://localhost:5173",
     'https://honeyary.vercel.app',
     "http://localhost:8080",
+    "http://0.0.0.0:8080",
+    "http://127.0.0.1:8080",
     "https://www.honeyary-ai.o-r.kr",
 ]
 app.add_middleware(
