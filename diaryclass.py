ed701f0f5141412dd16c53ec00ba2efd605f7fe1
lif_eposi <104194878+lifeposi@users.noreply.github.com>
2024-05-24 20:47:55 +0900

    requirements 수정

--- diaryclass.py
+++ diaryclass.py
@@ -8,6 +8,7 @@ from dotenv import load_dotenv
 from abc import ABC, abstractmethod
 from googleapiclient.discovery import build
 
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 load_dotenv(os.path.join(BASE_DIR, ".env"))
 
