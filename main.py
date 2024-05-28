8334ecf16e1f3137a28eca3c020236fa3fc28c6f
lif_eposi <104194878+lifeposi@users.noreply.github.com>
2024-05-28 11:24:53 +0900

    type error 수정

--- main.py
+++ main.py
@@ -464,7 +464,8 @@ async def get_youtube_playlist(request: Request):
 
     data = await request.json()
     member_id = request.headers.get('Authorization')
-    feelings = data.get
+    feelings = data.get('feelings', {})
+
 
     if member_id is None:
         return {
